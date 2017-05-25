649e444d0fb290daae4368f64e2359767b3c9e1e
aldyadk <aldy.andika@gmail.com>
2017-05-25 18:38:56 +0700

    nambah router get location viaISP

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const controllerUser = require('../controllers/users')
 const passport = require('passport')
 
+router.get('/signup',(req,res)=>{
+  res.render('signup')
+})
 router.post('/signup', controllerUser.signup)
 router.post('/signin', passport.authenticate('local', { session: false }), controllerUser.signin)
 
@@ -13,6 +16,17 @@ router.get('/getLocation',(req,res)=>{
 router.post('/getLocation',(req,res)=>{
   res.send(req.body)
 })
+
+router.get('/viaISP',(req,res)=>{
+    axios.get('http://ip-api.com/json')
+    .then(response=>{
+      res.send(response.data);
+      // console.log(response.data);
+    })
+    .catch(error=>{
+      res.send(error);
+    });
+  })
 
 
 module.exports = router
